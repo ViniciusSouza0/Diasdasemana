95b4e8bf4c031bbd175df8346fc502eb8f7170fd
aluno <vinicius.s.silveira.2015@gmail.com>
2024-03-04 19:35:46 -0300

    SwitchCase

---
+++ src/main/java/com/mycompany/switchcase/Switchcase.java
@@ -0,0 +1,60 @@
+package com.mycompany.switchcase;
+
+import java.util.Scanner;
+
+public class Switchcase {
+
+    public static void main(String[] args) {
+        
+Scanner teclado2 = new Scanner(System.in); 
+
+ int dia;
+ 
+ System.out.println("Digite os dias da semana");
+ dia = teclado2.nextInt();
+ 
+ 
+if(dia == 1) { System.out.println("Domingo");}
+
+if (dia == 2) { System.out.println("Segunda");}
+
+if (dia == 3) {System.out.println("Terca");}
+
+if (dia == 4) {System.out.println("Quarta");}
+
+if (dia == 5) {System.out.println("Quinta");}
+
+if (dia == 6) {System.out.println("Sexta");}
+
+if (dia == 7) {System.out.println("Sabado");}
+
+else {System.out.println("n invalidado");}
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
+
+
